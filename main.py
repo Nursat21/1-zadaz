a017a54615b95970af2e33680100e77317bde8c1
Nursat21 <149645066+Nursat21@users.noreply.github.com>
2023-11-02 09:19:54 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+
+num1=float(input("ведите первое число"))
+num2=float(input("ведите второе число"))
+
+sum_result=num1+num2
+difference=num1-num2
+product=num1*num2
+chastnoe=num1/num2
+
+print(f"сумма:{sum_result}")
+print(f"разность :{difference}")
+print(f"произведение:{product}")
+print(f"частное:{chastnoe}")
